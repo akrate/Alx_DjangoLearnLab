6b03b11293bde6725609c9987f466125fd5e2fca
akrate <oussamaakrate590@gmail.com>
2026-02-19 15:39:57 +0000

    test

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -2,11 +2,10 @@ from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
 
-# Get the custom user model
 User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
-    # Using serializers.CharField() for specific fields as requested
+    # Explicitly using CharField as requested
     username = serializers.CharField(max_length=150)
     password = serializers.CharField(write_only=True)
     bio = serializers.CharField(required=False, allow_blank=True)
@@ -16,13 +15,12 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ['id', 'username', 'email', 'password', 'bio', 'profile_picture']
 
     def create(self, validated_data):
-        # Using get_user_model().objects.create_user as requested
+        # Using get_user_model().objects.create_user for secure password hashing
         user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data.get('email'),
             password=validated_data['password'],
             bio=validated_data.get('bio', '')
         )
-        # Automatically create a token for the new user
         Token.objects.create(user=user)
         return user
--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -1,22 +1,18 @@
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    # Third party
+    # ...
     'rest_framework',
     'rest_framework.authtoken',
-    # Local apps
     'accounts',
+    'posts',
+    'django_filters',
 ]
 
-# Tell Django to use your custom user model
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'DEFAULT_PAGE_SIZE': 10,
 }
