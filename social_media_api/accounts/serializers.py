8069aa66c18460825b8a291cfac8193b8e2974a5
akrate <oussamaakrate590@gmail.com>
2026-02-19 15:32:35 +0000

    test

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -2,21 +2,27 @@ from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
 
+# Get the custom user model
 User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
+    # Using serializers.CharField() for specific fields as requested
+    username = serializers.CharField(max_length=150)
+    password = serializers.CharField(write_only=True)
+    bio = serializers.CharField(required=False, allow_blank=True)
+
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'bio', 'profile_picture', 'password')
-        extra_kwargs = {'password': {'write_only': True}}
+        fields = ['id', 'username', 'email', 'password', 'bio', 'profile_picture']
 
     def create(self, validated_data):
-        # Use create_user to hash the password automatically
-        user = User.objects.create_user(
+        # Using get_user_model().objects.create_user as requested
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data.get('email'),
             password=validated_data['password'],
-            bio=validated_data.get('bio', ''),
+            bio=validated_data.get('bio', '')
         )
+        # Automatically create a token for the new user
         Token.objects.create(user=user)
         return user
--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,8 +1,6 @@
-from rest_framework import generics, status
+from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
-from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.permissions import IsAuthenticated
 from .serializers import UserSerializer
 from django.contrib.auth import get_user_model
 
@@ -12,25 +10,15 @@ class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        user = serializer.save()
-        token = Token.objects.get(user=user)
-        return Response({
-            "user": UserSerializer(user).data,
-            "token": token.key
-        }, status=status.HTTP_201_CREATED)
-
-class LoginView(ObtainAuthToken):
     def post(self, request, *args, **kwargs):
-        response = super(LoginView, self).post(request, *args, **kwargs)
-        token = Token.objects.get(key=response.data['token'])
-        return Response({'token': token.key, 'user_id': token.user_id})
-
-class ProfileView(generics.RetrieveUpdateAPIView):
-    serializer_class = UserSerializer
-    permission_classes = [IsAuthenticated]
-
-    def get_object(self):
-        return self.request.user
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            # Retrieve the token created in the serializer's create method
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({
+                "token": token.key,
+                "user_id": user.pk,
+                "username": user.username
+            })
+        return Response(serializer.errors)
